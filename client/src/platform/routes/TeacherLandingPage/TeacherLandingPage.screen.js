81cf84ead5af6f0876a94059fc1a192dd3cfb9c4
Elearning.MNO <Mr.mostafa.farid@gmail.com>
2021-03-18 02:52:49 +0200

    more updates to the last screen
    updated the screen of the teachers landing page added the blue shapes with their hovers.

--- client/src/platform/routes/TeacherLandingPage/TeacherLandingPage.screen.js
+++ client/src/platform/routes/TeacherLandingPage/TeacherLandingPage.screen.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { AuthBtns } from "../../components";
+import { AuthBtns, BlueShape } from "../../components";
 
 //Styles
 import "./style.scss";
 
 //Assets
 import Logo from "../../../assets/img/logo.png";
-import { ReactComponent as FirstShape } from "../../../assets/img/teacher-landing-page-small-shapes/1.svg";
-import { ReactComponent as SecondShape } from "../../../assets/img/teacher-landing-page-small-shapes/2.svg";
-import { ReactComponent as ThirdShape } from "../../../assets/img/teacher-landing-page-small-shapes/3.svg";
-import { ReactComponent as FourthShape } from "../../../assets/img/teacher-landing-page-small-shapes/4.svg";
-import { ReactComponent as FifthShape } from "../../../assets/img/teacher-landing-page-small-shapes/5.svg";
+import Click from "../../../assets/img/teacher-landing-page-small-shapes/click (1).png";
+import Star from "../../../assets/img/teacher-landing-page-small-shapes/star.png";
+import Download from "../../../assets/img/teacher-landing-page-small-shapes/download.png";
+import Live from "../../../assets/img/teacher-landing-page-small-shapes/live.png";
+import Sheild from "../../../assets/img/teacher-landing-page-small-shapes/shield.png";
+// import { ReactComponent as FirstShape } from "../../../assets/img/teacher-landing-page-small-shapes/1.svg";
+// import { ReactComponent as SecondShape } from "../../../assets/img/teacher-landing-page-small-shapes/2.svg";
+// import { ReactComponent as ThirdShape } from "../../../assets/img/teacher-landing-page-small-shapes/3.svg";
+// import { ReactComponent as FourthShape } from "../../../assets/img/teacher-landing-page-small-shapes/4.svg";
+// import { ReactComponent as FifthShape } from "../../../assets/img/teacher-landing-page-small-shapes/5.svg";
 import { ReactComponent as TeacherLandingPageIllustration } from "../../../assets/img/teacher-landing-page-shape.svg";
 
 const TeacherLandingPage = () => {
@@ -28,7 +33,7 @@ const TeacherLandingPage = () => {
           <h2>نسعي لكي يمتد تأثيرك لأجيال قادمة </h2>
           <h5>
             نحن في <span>دروس أكاديمي</span> نوفر لك مميزات عديدة ، لن تجدها علي
-            أي منصة أخري <Link>اضغط هنا لمعرفة المزيد</Link>
+            أي منصة أخري <Link to="">اضغط هنا لمعرفة المزيد</Link>
           </h5>
         </div>
       </div>
@@ -40,6 +45,56 @@ const TeacherLandingPage = () => {
       </div>
       <div className="shapes-container">
         <div className="first-shape-container">
+          {" "}
+          <BlueShape
+            img={Star}
+            text1="موقع خاص بك"
+            text2="باسمك com. دومين
+لأنك مميز...سنجعل تميزك عالميا"
+          />
+        </div>
+        <div className="second-shape-container">
+          {" "}
+          <BlueShape
+            img={Click}
+            text1="بسهولة طرقعة الإصبع"
+            text2="لوحة تحكم سهلة
+            يمكنك متابعة الطلاب والحصص
+            وموزعي الحصص والدروس و الامتحانات
+            كل هذا من مكان واحد"
+          />
+        </div>
+        <div className="third-shape-container">
+          {" "}
+          <BlueShape
+            img={Download}
+            text1="لا يمكن لأحد تنزيل دروسك"
+            text2="دروسك في أمان تام
+            خبرائنا التقنيون يقومون بحماية
+            فيديوهاتك بأحدث البرمجيات"
+          />
+        </div>
+        <div className="fourth-shape-container">
+          {" "}
+          <BlueShape
+            img={Live}
+            text1="بث مباشر للحصص"
+            text2="نوفر لك ميزة البث المباشر
+            يمكنك بث الحصة لطلابك
+            صوت وصورة"
+          />
+        </div>
+        <div className="fifth-shape-container">
+          {" "}
+          <BlueShape
+            img={Sheild}
+            text1="مستوي عال من الأمان"
+            text2="خوادمنا علي مستوي عال من الأمان
+            وبياناتك وبيانات طلابك في أمان تام"
+          />
+        </div>
+
+        {/* <div className="first-shape-container">
           <FirstShape className="first-shape" />
         </div>
         <div className="second-shape-container">
@@ -53,7 +108,7 @@ const TeacherLandingPage = () => {
         </div>
         <div className="fifth-shape-container">
           <FifthShape className="fifth-shape" />
-        </div>
+        </div>*/}
       </div>
     </div>
   );
